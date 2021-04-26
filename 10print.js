221e628edb9e2b0a2ffe9c6c9fbe3dc3e7c1a7cf
Simren Kshetrapal <simrenk@uchicago.edu>
2021-04-26 12:33:46 -0500

    Getting undefined output

--- 10print.js
+++ 10print.js
@@ -5,14 +5,15 @@
 const w = process.stdout.columns
 
 function draw () {
-  setTimeout(draw, 1000)
+  setTimeout(draw, 1000/12)
+  const blocks = ['▓', '▒', '▚', '▜', '▏', '░']
+  // const blocks = ["U+0783"]
+  // const colors = ['\xlb[90m', '\xlb[92m']
   let output = ''
   for (let i = 0; i < w; i++) {
-    if (Math.random() > 0.5) {
-      output += '\\'
-    } else {
-      output += '/'
-    }
+    let b = Math.floor(Math.random * blocks.length)
+    // let c = Math.floor(Math.random * colors.length)
+    output += blocks[b]
   }
   console.log(output)
 }
